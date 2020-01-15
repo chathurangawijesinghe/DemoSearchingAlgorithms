8d3467e2a0158b0027f20d3c4630199621dcde25
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-01-15 10:59:22 +0800

    sln file

---
+++ DemoSearchingAlgorithms/DemoJumpSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSearchingAlgorithms
+{
+    public class DemoJumpSearch
+    {
+        public static int Search(int[] arr, int x)
+        {
+            var step = (int)Math.Sqrt(arr.Length);
+
+
+        }
+    }
+}
---
+++ DemoSearchingAlgorithms/DemoLinearSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSearchingAlgorithms
+{
+    public class DemoLinearSearch
+    {
+        public static int Search(int[] arr, int x)
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == x)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
