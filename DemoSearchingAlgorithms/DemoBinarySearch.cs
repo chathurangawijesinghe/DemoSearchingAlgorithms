bbb60f9cc4558cfe9596f9bb86a3cfef68dbe319
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-02-06 11:18:29 +0800

    remove ignored files

---
+++ DemoSearchingAlgorithms/DemoBinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSearchingAlgorithms
+{
+    public class DemoBinarySearch
+    {
+        public static int Search(int[] arr, int start, int end, int x)
+        {
+            if (end >= start)
+            {
+                var middle = (start + end) / 2;
+                if (arr[middle] == x)
+                {
+                    return middle;
+                }
+
+                if (arr[middle] > x)
+                {
+                    return Search(arr, start, middle - 1, x);
+                }
+                else
+                {
+                    return Search(arr, middle + 1, end, x);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
